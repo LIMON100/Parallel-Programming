c1f04ddade71f611241a2ab1c0a78c5ce2b60afe
mahmudur320 <mahmudurlimon41@gmail.com>
2020-04-30 22:24:16 +0600

    add file

---
+++ sendingAndRecivingdata.py
@@ -0,0 +1,20 @@
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+
+rank = comm.rank
+size = comm.size
+name = MPI.Get_processor_name()
+
+
+shared = (rank + 1) * 5
+
+if rank == 0:
+    data = shared
+    comm.send(data , des = 1)
+    print("From rank " , rank ," we sent " , data)
+    
+
+elif rank == 1:
+    data = comm.recv(source = 0)
+    print("on node " , rank ," we recived " , data)
