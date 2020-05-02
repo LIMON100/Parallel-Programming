da3bd42157d6e54d5bc62ffe9d883a395e04c18e
mahmudur320 <mahmudurlimon41@gmail.com>
2020-05-02 12:29:46 +0600

    Update file

--- Thread/Hellowiththread.py
+++ Thread/Hellowiththread.py
@@ -1,3 +1,31 @@
 from threading import Thread
 from time import sleep
 
+class bokk(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.messege = "Python Parallel programming started.\n"
+        
+    
+    def print_messege(self):
+        print(self.messege)
+        
+        
+    def run(self):
+        print("Thread Starting")
+        
+        x = 0
+        while(x < 10):
+            self.print_messege()
+            sleep(2)
+            x = x + 1
+        
+        print("Thread Ended..\n")
+        
+print("Process Start..\n")
+
+b = bokk()
+
+b.start()
+
+print("Process Ended..\n")
